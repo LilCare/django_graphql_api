0a9d71a5e2acff14667f29e24ef3e8cee1e21bd1
Lily Carey <lilcare@Lilys-MacBook-Air.local>
2020-01-29 17:52:13 -0800

    Implement simple polls app and graphql app with Http response

---
+++ mysite/graphql/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+  return HttpResponse('Here is the graphql index')
+
+def userLookup(request, username):
+  print(username)
+  return HttpResponse(username)
